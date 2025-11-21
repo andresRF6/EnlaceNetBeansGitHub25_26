ef644748b09331c15bfa11b71c4c7f27c78fbbdc
achillon <andres.chillonsesma@educa.madrid.org>
2025-11-21 13:17:36 +0100

    Mi primer commit desde NB

---
+++ PRUEBASn_gh/src/pruebasn_gh/Otra.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pruebasn_gh;
+
+/**
+ *
+ * @author andres.chillon
+ */
+public class Otra {
+    public static void main(String[] args) {
+        System.out.println("Adios");
+    }
+}
---
+++ PRUEBASn_gh/src/pruebasn_gh/Saludo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pruebasn_gh;
+
+/**
+ *
+ * @author andres.chillon
+ */
+public class Saludo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
